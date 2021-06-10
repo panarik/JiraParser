0b4bbbae786346a4dc4c5910dca178918b2dcba6
panarik <panarik@yandex.ru>
2021-06-11 01:04:57 +0300

    fix issues JSON parse

--- src/main/java/GetIssue.java
+++ src/main/java/GetIssue.java
@@ -1,13 +1,12 @@
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import parse.IssueList;
 
-import java.io.DataInput;
 import java.io.IOException;
 
 public interface GetIssue {
@@ -20,9 +19,9 @@ public interface GetIssue {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(URL+ISSUEPATH+issueKey)
+                .url(URL + ISSUEPATH + issueKey)
                 .method(GET, null)
-                .addHeader("Authorization", "Basic "+authToken)
+                .addHeader("Authorization", "Basic " + authToken)
                 .build();
         System.out.println(request);
         Response response = client.newCall(request).execute();
@@ -34,9 +33,9 @@ public interface GetIssue {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(URL+ISSUEPATH+issueKey+"/changelog?startAt=0&maxResults=100")
+                .url(URL + ISSUEPATH + issueKey + "/changelog?startAt=0&maxResults=100")
                 .method(GET, null)
-                .addHeader("Authorization", "Basic "+authToken)
+                .addHeader("Authorization", "Basic " + authToken)
                 .build();
         System.out.println(request);
         Response response = client.newCall(request).execute();
@@ -50,23 +49,22 @@ public interface GetIssue {
         Request request = new Request.Builder()
                 .url("https://panariks.atlassian.net/rest/api/2/search?jql=project=TEST&fields=issue&startAt=0&maxResults=8000")
                 .method(GET, null)
-                .addHeader("Authorization", "Basic "+authToken)
+                .addHeader("Authorization", "Basic " + authToken)
                 .build();
         Response response = client.newCall(request).execute();
+        String issuesInString = response.body().string();
 
         //выводим статус
-        System.out.println("\n"+request);
+        System.out.println("\n" + request);
         System.out.println(response);
-        System.out.println(response.body().string());
+        System.out.println("Тело JSON:");
+        System.out.println(issuesInString);
 
         //парсим на объекты
         ObjectMapper mapper = new ObjectMapper();
-        String issuesInString = response.body().toString();
 
-        //ToDo дебажим парсинг (эталон JSONа в файлике тут: src/main/resources/exampleIssueList.json)
         IssueList issueList = mapper.readValue(issuesInString, IssueList.class);
     }
-
 
 
 }
