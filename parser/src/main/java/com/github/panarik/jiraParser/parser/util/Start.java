fcc50e765c2f9c5144b98657b53d9010337e139c
panarik <panarik@yandex.ru>
2021-10-10 22:54:15 +0300

    delete task

--- parser/src/main/java/com/github/panarik/jiraParser/parser/util/Start.java
+++ parser/src/main/java/com/github/panarik/jiraParser/parser/util/Start.java
@@ -15,62 +15,7 @@ public class Start {
 
         //домашка п.2
         manager = Executors.newCachedThreadPool();
-        manager.execute(Start::task1); //домашка п.1
         manager.execute(Server::run); //запуск сервера
         manager.execute(Parser::run); //запуск парсера Jira
     }
-
-    private static void task1() {
-        for (int i = 0; i<5; i++) {
-            new Thread(() -> printA()).start();
-            new Thread(() -> printB()).start();
-            new Thread(() -> printC()).start();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Log.debug(e.getMessage());
-            }
-        }
-    }
-
-    private static synchronized void printA() {
-        try {
-            if (letter.equals(" ") | letter.equals("C")) {
-                letter = "A";
-                System.out.println(letter);
-                Thread.sleep(500);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static synchronized void printB() {
-        try {
-            if (letter.equals("A")) {
-                letter = "B";
-                System.out.println(letter);
-                Thread.sleep(500);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static synchronized void printC() {
-        try {
-            if (letter.equals("B")) {
-                letter = "C";
-                System.out.println(letter);
-                Thread.sleep(500);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-
 }
